f0c605682827e9e0e7a0208112fa72dc0ece85a3
osirissc2 <burstein.alan@gmail.com>
2017-03-25 15:16:37 -0400

    Firebase threaded

--- app/src/main/java/com/example/ivorine/pctapp/LoginActivity.java
+++ app/src/main/java/com/example/ivorine/pctapp/LoginActivity.java
@@ -3,11 +3,18 @@ package com.example.ivorine.pctapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+
         setContentView(R.layout.activity_login);
        }
 }
