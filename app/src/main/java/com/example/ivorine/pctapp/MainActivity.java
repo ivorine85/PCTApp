69e9e857027e12bfeff4dfb8667ae9d2d7630c5e
osirissc2 <burstein.alan@gmail.com>
2017-03-25 21:46:03 -0400

    Broke/Improved main

--- app/src/main/java/com/example/ivorine/pctapp/MainActivity.java
+++ app/src/main/java/com/example/ivorine/pctapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ivorine.pctapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,10 +12,37 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button mGoToLogin, Events, Brothers, Jobs, Home;
 
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_login:
+                    mTextMessage.setText(R.string.title_login);
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intent);
+
+                    return true;
+            }
+            return false;
+        }
+
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,17 +50,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mGoToLogin= (Button) findViewById(R.id.Login);
-        mGoToLogin.setOnClickListener(this);
-
-        Events = (Button) findViewById(R.id.Events);
-        Events.setOnClickListener(this);
-
-        Brothers = (Button) findViewById(R.id.Brothers);
-        Brothers.setOnClickListener(this);
-
-        Jobs = (Button) findViewById(R.id.Jobs);
-        Jobs.setOnClickListener(this);
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         Home = (Button) findViewById(R.id.Home);
         Home.setOnClickListener(this);
@@ -56,6 +77,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
@@ -67,26 +89,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         int i = v.getId();
-        if(i == R.id.Login){
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
+
+        if(i == R.id.Home){
+//            Intent intent = new Intent(this, MainActivity.class);
+//            startActivity(intent);
         }
-        else if (i== R.id.Events) {
-            Intent intent = new Intent(this, Events.class);
-            startActivity(intent);
-        }
-        else if (i== R.id.Jobs) {
-            Intent intent = new Intent(this, Jobs.class);
-            startActivity(intent);
-        }
-        else if (i== R.id.Brothers) {
-            Intent intent = new Intent(this, Brothers.class);
-            startActivity(intent);
-        }
-        else if (i== R.id.Home) {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        }
+//        else if (i== R.id.Events) {
+//            Intent intent = new Intent(this, Events.class);
+//            startActivity(intent);
+//        }
+//        else if (i== R.id.Jobs) {
+//            Intent intent = new Intent(this, Jobs.class);
+//            startActivity(intent);
+//        }
+//        else if (i== R.id.Brothers) {
+//            Intent intent = new Intent(this, Brothers.class);
+//            startActivity(intent);
+//        }
+//        else if (i== R.id.Home) {
+//            Intent intent = new Intent(this, MainActivity.class);
+//            startActivity(intent);
+//        }
+//        else if(i==R.id.navigation){
+//            Intent intent = new Intent(this, LoginActivity.class);
+//            startActivity(intent);
+//        }
+//        else if(i==R.id.navigation_login){
+//            Intent intent = new Intent(this, LoginActivity.class);
+//            startActivity(intent);
+//        }
     }
 
 }
